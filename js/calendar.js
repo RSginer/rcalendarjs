8ae65955d9752a167268493ef6275397bfa2ff17
Ruben Soler <r.solerginer@gmail.com>
2016-10-05 21:15:03 +0200

    muestra dias

--- js/calendar.js
+++ js/calendar.js
@@ -5,9 +5,20 @@ initCalendar(date);
 
 function initCalendar(date) {
     var header = document.getElementById("header");
+    var body = document.getElementById("body");
     var days = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
     var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
     header.innerHTML = months[date.getMonth()] + " - " + date.getFullYear();
+    var monthDays = [31,28,31,30,31,30,31,31,30,31,30,31]
+    if (isLeap(date.getFullYear())) {
+        monthDays[1]=29;
+    }
+    var text="";
+    for (var i = 1; i <= monthDays[date.getMonth()]; i++) {
+       text += '<div class="calendar_body_week_day">'+i+'</div>';
+    }
+    
+    body.innerHTML = text;
 }
 
 function addMonth() {
@@ -35,4 +46,8 @@ function subMonth() {
         this.date = new Date(this.date.getFullYear(), (this.date.getMonth() - 1), 01);
     }
     initCalendar(this.date);
+}
+
+function isLeap(year){
+return ((year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0)));
 }
