91941aef20494a004899b7f19958f95282fe9bf8
Ruben Soler <r.solerginer@gmail.com>
2016-10-06 21:32:29 +0200

    refactorizando

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,39 +1,50 @@
 var showOrHide = false;
 var today = new Date();
 var date = new Date(today.getFullYear(), today.getMonth(), 01);
+
+document.getElementsByTagName("rcalendar")[0].innerHTML =
+        '<button class="btn btn--show" onclick="show()"><i class="fa fa-calendar"></i> Calendario</button>'+
+        '<span id="info-message" class="info-message"><i class="fa fa-reply"></i> Click para mostrar </span>' +
+        '<div id="content-calendar" class="hide"></div>';
+
 initCalendar(date);
 
 function initCalendar(date) {
-    document.getElementById("content-calendar").innerHTML=
-            '<div class="right"><button class="btn btn__add-month" onclick="addMonth()">Siguiente</button></div>'+
-                '<div class="left"><button class="btn btn__sub-month" onclick="subMonth()">Anterior</button></div>  <br>'+
-                '<div id="calendar" class="calendar">'+
-                    '<div id="header" class="calendar__header">MES</div>' +
-                    '<div class="calendar__body">'+
-                       ' <div class="calendar__days">'+
-                            '<div class="calendar__day">L</div>'+
-                            '<div class="calendar__day">M</div>'+
-                           ' <div class="calendar__day">X</div>'+
-                            '<div class="calendar__day">J</div>'+
-                            '<div class="calendar__day">V</div>'+
-                            '<div class="calendar__day">S</div>'+
-                           ' <div class="calendar__day">D</div>'+
-                       ' </div>'+
-                        '<div id="body" class="calendar__week">'+
-                       ' </div>'+
-                   ' </div>'+
-                '</div>';
+
+    document.getElementById("content-calendar").innerHTML =
+            '<div class="right"><button class="btn btn__add-month" onclick="addMonth()">Siguiente</button></div>' +
+            '<div class="left"><button class="btn btn__sub-month" onclick="subMonth()">Anterior</button></div>  <br>' +
+            '<div id="calendar" class="calendar">' +
+            '<div id="header" class="calendar__header">MES</div>' +
+            '<div class="calendar__body">' +
+            '<div class="calendar__days">' +
+            '<div class="calendar__day">L</div>' +
+            '<div class="calendar__day">M</div>' +
+            '<div class="calendar__day">X</div>' +
+            '<div class="calendar__day">J</div>' +
+            '<div class="calendar__day">V</div>' +
+            '<div class="calendar__day">S</div>' +
+            '<div class="calendar__day">D</div>' +
+            '</div>' +
+            '<div id="body" class="calendar__week">' +
+            '</div>' +
+            '</div>' +
+            '</div>';
+    
     var header = document.getElementById("header");
     var body = document.getElementById("body");
     var days = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
     var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-    header.innerHTML = months[date.getMonth()] + " - " + date.getFullYear();
     var monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    var text = "";
+    var numBlankDivs;
+    
+    header.innerHTML = months[date.getMonth()] + " - " + date.getFullYear();
+
     if (isLeap(date.getFullYear())) {
         monthDays[1] = 29;
     }
-    var text = "";
-    var numBlankDivs;
+
     if (date.getDay() != 0) {
         numBlankDivs = date.getDay();
     } else {
@@ -43,7 +54,7 @@ function initCalendar(date) {
         text += '<div class="calendar__week__day"></div>';
     }
     for (var i = 1; i <= monthDays[date.getMonth()]; i++) {
-        if (today.getDate() == i && today.getMonth() == date.getMonth() && today.getFullYear()==date.getFullYear()) {
+        if (today.getDate() == i && today.getMonth() == date.getMonth() && today.getFullYear() == date.getFullYear()) {
             text += '<div class="calendar__week__day calendar__week__day--current-day">' + i + '</div>';
         } else {
             text += '<div class="calendar__week__day">' + i + '</div>';
@@ -78,11 +89,11 @@ function isLeap(year) {
 function show() {
     if (this.showOrHide == true) {
         document.getElementById("content-calendar").style = "visibility:hidden";
-        this.showOrHide=false;
+        this.showOrHide = false;
         document.getElementById("info-message").innerHTML = '<i class="fa fa-reply"></i> Click para mostrar';
     } else {
         document.getElementById("content-calendar").style = "visibility:visible";
-        this.showOrHide=true;
+        this.showOrHide = true;
         document.getElementById("info-message").innerHTML = '<i class="fa fa-reply"></i> Click para ocultar';
     }
 }
